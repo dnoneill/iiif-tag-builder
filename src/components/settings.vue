a9d37be39be58d9ce5ddcad1890f1bf2061c7166
Niqui O'Neill <doneill@ncsu.edu>
2021-07-12 13:47:04 -0400

    update source code

--- src/components/settings.vue
+++ src/components/settings.vue
@@ -36,9 +36,24 @@ export default {
   },
   created() {
       console.log('created')
+      
       var settings = booleanoptions[this.type]()
       settings = settings.concat(freetext[this.type]())
       settings = settings.concat(dropdowns[this.type]())
+      if (this.type.indexOf('storyboard') > -1){
+          settings.append({'name': 'additionalinfo',
+          'option': 'id for HTML object',
+          'description': 'Is the id for an HTML object whose innerHTML will be loaded into the info tab. This HTML object should have a title tag.'})
+          settings.append({'name': 'tagscolor',
+          'option': 'JSON object',
+          'description': "String in JSON format. Should have tag with corresponding color. Any CSS color can be used. i.e. {'tag name': 'color', 'tag name 2': 'color2'}. See single annotation settings for an example."})
+          settings.append({'name': 'overlaycolor',
+          'option': 'Hex or RGB color',
+          'description': "change the overlay color; default is #00bfff. See single annotation settings for an example."})
+          settings.append({'name': 'activecolor',
+          'option': 'Hex or RGB color',
+          'description': "change active color; default is lightgreen. See single annotation settings for an example."})
+      }
       this.settings = _.sortBy(settings, 'name');
       console.log(this.settings)
   }
